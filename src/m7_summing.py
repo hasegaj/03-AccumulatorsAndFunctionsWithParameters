b63a4a96a95b8eafe428cc1553eb3d69aeb8ffc6
hasegaj <hasegaj@rose-hulman.edu>
2018-12-02 15:06:34 -0500

    done

--- src/m7_summing.py
+++ src/m7_summing.py
@@ -4,8 +4,8 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Josiah Hasegawa.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -17,7 +17,7 @@ def main():
 def run_test_sum_cosines():
     """ Tests the   sum_cosines   function. """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # Done: 2. Implement this function.
     #   It TESTS the  sum_cosines  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -28,6 +28,23 @@ def run_test_sum_cosines():
     print('--------------------------------------------------')
     print('Testing the   sum_cosines   function:')
     print('--------------------------------------------------')
+    #test1
+    expected = 1
+    answer = sum_cosines(1)
+    print('test 1 expected', expected)
+    print('     actual: ', answer)
+
+    #test2
+    expected = 2
+    answer = sum_cosines(4)
+    print('test 2 expected', expected)
+    print('     actual: ', answer)
+
+    #test3
+    expected = 3
+    answer = sum_cosines(10)
+    print('test 3 expected', expected)
+    print('     actual: ', answer)
 
 
 def sum_cosines(n):
@@ -39,21 +56,29 @@ def sum_cosines(n):
     Example:
       If n is 3, this function returns
         cos(0) + cos(1) + cos(2) + cos(3)   which is about 0.13416.
+        josiah: incorrect; it should be n = 4 gets .13416
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # Done: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
     #   No fair running the code of  sum_cosines  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # -------------------------------------------------------------------------
+    import math
+    t = 0
+    for k in range(n):
+        t = t + math.cos(k)
+
+    return t
+
 
 
 def run_test_sum_square_roots():
     """ Tests the   sum_square_roots   function. """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # Done: 4. Implement this function.
     #   It TESTS the  sum_square_roots  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -64,6 +89,24 @@ def run_test_sum_square_roots():
     print('--------------------------------------------------')
     print('Testing the   sum_square_roots   function:')
     print('--------------------------------------------------')
+
+    #test1
+    expected = 11.854408
+    answer = sum_square_roots(6)
+    print('test 1 expected', expected)
+    print('     actual: ', answer)
+
+    #test2
+    expected = 19.060167
+    answer = sum_square_roots(8)
+    print('test 2 expected', expected)
+    print('     actual: ', answer)
+
+    #test3
+    expected = 27.30280778
+    answer = sum_square_roots(10)
+    print('test 3 expected', expected)
+    print('     actual: ', answer)
 
 
 def sum_square_roots(n):
@@ -79,8 +122,15 @@ def sum_square_roots(n):
          sqrt(2) + sqrt(4) + sqrt(6) + sqrt(8) + sqrt(10),
       which is about 11.854408.
     """
+    import math
+    r = 0
+    for k in range(n):
+        r = r + math.sqrt(2*k)
+
+    return r
+
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # Done: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
